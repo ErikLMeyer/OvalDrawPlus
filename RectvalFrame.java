7e1c0e438a4d7dda684f262aa9c16f4de4e6bfce
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-10 14:11:56 -0600

    Added constructor with parameters

--- RectvalFrame.java
+++ RectvalFrame.java
@@ -7,13 +7,26 @@ public class RectvalFrame extends JFrame {
     static final int R_RED = 22, R_GREEN = 2, R_BLUE = 159;
     static final int O_RED = 0, O_GREEN = 255, O_BLUE = 0;
 	
-	// Constructor for the class
+	// Default constructor for the class
     RectvalFrame(){
         setTitle("The frame for this project");
         setBounds(250,150,400,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         TwoShapes rO = new TwoShapes(R_RED, R_GREEN, R_BLUE, O_RED, O_GREEN, O_BLUE);
+       
+        Container paneForContent = getContentPane();
+
+        paneForContent.add(rO);
+    }
+
+    // Constructor for class
+    RectvalFrame(int rR, int rG, int rB, int oR, int oG, int oB){
+        setTitle("The frame for this project");
+        setBounds(250,150,400,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        TwoShapes rO = new TwoShapes(rR, rG, rB, oR, oG, oB);
        
         Container paneForContent = getContentPane();
 
